68f270e0e1b9dabcfc3df52bbe8a51989c941fed
Michael Pascucci <mdpascucci@proton.me>
2025-08-30 07:55:03 -0400

    Initial Commit

---
+++ Bookzzle/BookzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookzzleApp.swift
+//  Bookzzle
+//
+//  Created by Michael on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BookzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
